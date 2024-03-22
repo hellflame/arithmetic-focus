ede136b935dd5ec28a074c386d660f14f5d04ff8
hellflame <hellflamedly@gmail.com>
2024-03-22 21:45:26 +0800

    remove debug

--- entry.go
+++ entry.go
@@ -13,7 +13,6 @@ var pages embed.FS
 func main() {
 	args := utils.ParseArgs()
 	if args != nil {
-		fmt.Println("args", args)
 		address := args.GetAddress()
 		if !args.NoBrowser {
 			utils.OpenExplore(fmt.Sprintf("http://%s", address))
